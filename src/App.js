2a95ec264956355b67b5c1a6847aadd6020d52fd
Sailor Winkelman <sailor.winkelman@modop.com>
2019-11-15 00:25:37 -0800

    Connect to firestore

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import  { FirebaseContext } from './Firebase';
 
 const App = () => (
   <FirebaseContext.Consumer>
-   {firebase => (<div>I have access to firebase</div>)}
+   {firebase => {
+      firebase.db.collection('users').doc('NLi76ZESLb5ZVcUfqflw')
+      .delete()
+      .then(() => console.log('deleted'))
+      .catch((err) => console.log(err))
+
+     return <div>
+       <input></input>
+     </div>
+     
+   }}
 </FirebaseContext.Consumer>
 );
 export default App;
