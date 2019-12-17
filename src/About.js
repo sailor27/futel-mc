c315bcd06e2ea52db3ba1e0485790b2a9069c4a4
Sailor Winkelman <sailor.winkelman@modop.com>
2019-12-16 21:11:13 -0800

    add background triangles to about page

--- src/About.js
+++ src/About.js
@@ -1,8 +1,9 @@
 /** @jsx jsx */
-import {ABOUT_CONTENT} from './constants/content';
 import * as TreeImg from './img/wishing_tree.jpeg';
 import * as PhoneImg from './img/futel_phone_clinton.jpeg';
-import Svg from './Svg'
+import {colors} from './constants/colors';
+import {ReactComponent as PhoneSvg} from './img/blue-phone.svg';
+import {ReactComponent as Triangle} from './img/triangle.svg';
 import {jsx, css} from '@emotion/core';
 
 function About() {
@@ -18,6 +19,7 @@ function About() {
 
           h3 {
             font-weight: 800;
+            text-transform: uppercase;
           }
 
           .row {
@@ -25,67 +27,99 @@ function About() {
             flex-flow: row nowrap;
             @media (max-width: 768px) {
               flex-flow: row wrap;
+              justify-content: space-around;
             }
             margin-bottom: 30px;
           }
           .content {
+            margin-right: 30px;
+            @media (max-width: 768px) {
+              margin: 0;
+            }
             flex-basis: 550px;
-            & p:first-child {
+            & p:first-of-type {
               margin: auto;
             }
           }
           .image {
+            @media (max-width: 768px) {
+              margin: 10px;
+            }
             @media (max-width: 420px) {
               display: none;
             }
+            display: flex;
             > img {
               max-width: 550px;
-              max-height: 550px;
+              max-height: 450px;
             }
           }
         }
       `}
     >
       <h1>About Futel Missed Connections</h1>
+      <Triangle
+        css={{
+          maxWidth: `450px`,
+          zIndex: `-1`,
+          position: `fixed`,
+          top: `20px`,
+          filter: `brightness(150%)`,
+        }}
+      />
       <div className="row">
-        <div className="image" css={{marginRight: '30px'}}>
-          <img src={PhoneImg} alt="futel phone" />
-        </div>
-
         <div className="content">
-          <h3>What?</h3>
           <p>
             <b>Missed connections </b> are short messages, posted in public,
             which explore unfulfilled potentials and create a second chance for
             people to connect.
           </p>
           <p>
-            Futel Missed Connections is a public messaging service presented by
-            Sailor Winkelman in collaboration with Futel for Open Signal's
-            <i> Hold the Phone</i> project. Users will be able to record, listen
-            to, and reply to missed connections from a public telephone.
+            <span css={{color: colors.darkPurple}}>
+              Futel Missed Connections
+            </span>{' '}
+            is a public messaging service presented by Sailor Winkelman in
+            collaboration with Futel for Open Signal's
+            <em> Hold the Phone </em> project. Users will be able to record,
+            listen to, and reply to missed connections from a public telephone.
           </p>
-          <h3>When/Where?</h3>
-          <p>
-            The Futel Missed Connections phone interface will go live on the
-            Futel phone outside Open Signal on
-            <a href="https://g.page/opensignalpdx?share"> NE MLK & Grand</a> in
-            August 2020.
-          </p>
-          <p>
-            You can record your message <a href="/"> on the web </a> starting
-            <i> now</i> to have it heard on the Futel phone starting in August.
-          </p>
+          <br />
+          <div
+            css={{
+              border: `2px solid black`,
+              borderRadius: `15px`,
+              padding: `15px`,
+            }}
+          >
+            <p>
+              The Futel Missed Connections phone interface will go live on the
+              Futel phone outside Open Signal on
+              <a href="https://g.page/opensignalpdx?share">
+                {' '}
+                NE MLK & Grand
+              </a>{' '}
+              in August 2020.
+            </p>
+            <p css={{marginBottom: 0}}>
+              You can record your message <a href="/"> on the web </a> starting
+              <em> now</em> to have it heard on the Futel phone starting in
+              August.
+            </p>
+          </div>
+        </div>
+
+        <div className="image" css={{marginLeft: '30px'}}>
+          <img src={PhoneImg} alt="futel phone" />
         </div>
       </div>
-      <div className="row" css={{flexFlow: 'row wrap'}}>
-        <div className="content" css={{marginRight: '30px'}}>
-          <h3>Why?</h3>
+
+      <div className="row">
+        <div className="content">
           <p>
-            Missed Connections originated in the Personals sections of local
+            Missed Connections started in the Personals sections of local
             newspapers and were popularized online with craigslist. The
             anonymous, local format draws out the desires, dreams, and imagined
-            potentials felt between people who share spaces like busses,
+            potentials felt between people who share spaces like buses,
             sidewalks, and parks.
           </p>
 
@@ -101,23 +135,57 @@ function About() {
             new understanding of each other to get in touch, share resources,
             and re-imagine the local world.
           </p>
-
+        </div>
+        <div className="image" css={{marginLeft: '30px'}}>
+          <PhoneSvg css={{minWidth: `400px`}} />
+        </div>
+      </div>
+      <div className="row" css={{flexFlow: 'row wrap'}}>
+        <div className="content" css={{marginRight: '30px'}}>
           <p>
             Futel Missed Connections was inspired by its text-based counterparts
-            and by{' '}
+            and by
             <a href="https://www.google.com/maps/place/The+Wishing+Tree/@45.5445637,-122.6592466,19z/data=!4m5!3m4!1s0x5495a73822599995:0x322a7a12ac77c53a!8m2!3d45.5445211!4d-122.6584831">
-              The Wishing Tree on 7th Ave
+            {' '}The Wishing Tree on 7th Ave
             </a>
             , located only a few blocks from Open Signal, which invites
-            passersby to hand write wishes on tags and hang them from a tree.{' '}
+            passersby to hand write wishes on tags and hang them from a tree.
           </p>
         </div>
 
         <div className="image">
-          <Svg/>
-          {/* <img alt="wishing tree" src={TreeImg} /> */}
+          <img alt="wishing tree" src={TreeImg} />
         </div>
       </div>
+      <Triangle
+        css={css`
+          @media (max-width: 420px) {
+            display: none;
+          }
+          max-width: 380px;
+          z-index: -1;
+          position: fixed;
+          bottom: 0;
+          right: 0;
+          margin: 60px;
+          filter: brightness(150%);
+          transform: scaleX(-1);
+        `}
+      />
+      <Triangle
+        css={css`
+          @media (max-width: 768px) {
+            display: none;
+          }
+          max-width: 300px;
+          z-index: -1;
+          position: fixed;
+          bottom: 0;
+          left: 0;
+          filter: brightness(150%);
+          transform: scaleX(-1);
+        `}
+      />
     </div>
   );
 }
--- src/Svg.js
+++ src/Svg.js
@@ -1,14 +1,21 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core';
+import { jsx, css } from '@emotion/core';
 import { ReactComponent as PhoneSvg } from './img/purple-phone.svg';
 import { colors } from './constants/colors';
 
 export const Svg = ({
-  fill = colors.lightPurple,
-  width = "100%",
-  className = "",
-  viewBox = "0 0 32 32"
-}) => (<PhoneSvg width={width}/>)
+         fill = colors.lightPurple,
+         className = '',
+         viewBox = '0 0 32 32',
+       }) => (
+         <PhoneSvg
+           css={css`
+            {
+              min-width: 400px;
+            }
+           `}
+         />
+       );
 
 export default Svg;
 
